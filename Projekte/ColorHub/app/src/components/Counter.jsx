e174f581ccd51cc6d4a2003f22cadbd3900545a0
Thomas Margowski <t.margowski@fis-gmbh.de>
2023-02-03 10:08:21 +0100

    react app

--- Projekte/ColorHub/app/src/components/Counter.jsx
+++ Projekte/ColorHub/app/src/components/Counter.jsx
@@ -4,13 +4,26 @@ class Counter extends Component {
   state = {
     count: 0,
   };
+
+  styles = {
+    fontSize: 15,
+    fontWeight: "bold",
+  };
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.state.count}</span>
-        <button>Increment</button>
+        <span style={this.styles} className="badge bg-secondary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-warning btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
